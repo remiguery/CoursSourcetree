c6db9d6adf7b7ce5c5065fdef4abeb46da8e7a02
Quentin <hello@qhaensler.com>
2018-03-02 11:43:36 +0100

    Fix JS

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-$('body').prepend('<img src="./supdeweb-logo.jpg" alt="Sup de Web" style="width: 350px; margin: 50px 0 50px 0;" class="img-rounded">');
+$('document').ready(function(){
+
+  $('body').prepend('<img src="./supdeweb-logo.jpg" alt="Sup de Web" style="width: 350px; margin: 50px 0 50px 0;" class="img-rounded">');
+
+});
